f5cdbe319605f66678e92cc525d8708dd3f17050
Caroline Maynard <cem@php.net>
2005-07-28 13:59:18 +0000

    Sort out include ordering to satisfy MS compiler
    git-svn-id: http://svn.php.net/repository/pecl/sdo/trunk@191722 c90b9560-bf6c-de11-be94-00142212c4b1

--- sdolib/src/xmldas/SDOXSDWriter.h
+++ sdolib/src/xmldas/SDOXSDWriter.h
@@ -1,22 +1,22 @@
-/* 
+/*
 +----------------------------------------------------------------------+
-| (c) Copyright IBM Corporation 2005.                                  | 
+| (c) Copyright IBM Corporation 2005.                                  |
 | All Rights Reserved.                                                 |
-+----------------------------------------------------------------------+ 
-|                                                                      | 
-| Licensed under the Apache License, Version 2.0 (the "License"); you  | 
-| may not use this file except in compliance with the License. You may | 
-| obtain a copy of the License at                                      | 
++----------------------------------------------------------------------+
+|                                                                      |
+| Licensed under the Apache License, Version 2.0 (the "License"); you  |
+| may not use this file except in compliance with the License. You may |
+| obtain a copy of the License at                                      |
 |  http://www.apache.org/licenses/LICENSE-2.0                          |
-|                                                                      | 
-| Unless required by applicable law or agreed to in writing, software  | 
-| distributed under the License is distributed on an "AS IS" BASIS,    | 
-| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      | 
-| implied. See the License for the specific language governing         | 
-| permissions and limitations under the License.                       | 
-+----------------------------------------------------------------------+ 
-| Author: Pete Robbins                                                 | 
-+----------------------------------------------------------------------+ 
+|                                                                      |
+| Unless required by applicable law or agreed to in writing, software  |
+| distributed under the License is distributed on an "AS IS" BASIS,    |
+| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+| implied. See the License for the specific language governing         |
+| permissions and limitations under the License.                       |
++----------------------------------------------------------------------+
+| Author: Pete Robbins                                                 |
++----------------------------------------------------------------------+
 
 */
 
@@ -24,6 +24,7 @@
 #define _SDOXSDWRITER_H_
 #include <libxml/xmlwriter.h>
 #include "SDOXMLString.h"
+#include "DataObject.h"
 #include "DASDataFactory.h"
 
 
@@ -33,22 +34,22 @@ namespace commonj
 	{
 		namespace xmldas
 		{
-			
+
 			class SDOXSDWriter
 			{
-				
+
 			public:
-				
+
 				SDOXSDWriter(DASDataFactoryPtr dataFactory = NULL);
-				
+
 				virtual ~SDOXSDWriter();
-				
+
 				int write(const TypeList& types, const SDOXMLString& targetNamespaceURI);
 
 			protected:
 				void setWriter(xmlTextWriterPtr textWriter);
 				void freeWriter();
-				
+
 			private:
 				xmlTextWriterPtr writer;
 
@@ -57,7 +58,7 @@ namespace commonj
 				DASDataFactoryPtr	dataFactory;
 
 				SDOXMLString resolveName(const SDOXMLString& uri, const SDOXMLString& name, const SDOXMLString& targetNamespaceURI);
-				
+
 			};
 		} // End - namespace xmldas
 	} // End - namespace sdo
