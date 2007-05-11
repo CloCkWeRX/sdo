b03eae05cedcf0f86de253fa6c50841f0402cf24
Graham Charters <gcc@php.net>
2007-05-11 13:12:59 +0000

    Added samples for eBay soap binding.
    git-svn-id: http://svn.php.net/repository/pecl/sdo/trunk@235415 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ examples/SCA/ebaysoap/eBayClient2.php
@@ -0,0 +1,52 @@
+<?php
+
+include 'SCA/SCA.php';
+
+function getResults($query) {
+
+    $ebay_consumer = SCA::getService('eBaySvc.wsdl', 'ebaysoap', 
+                                     array('config' => './config/ebay.ini'));
+
+    // Create the body
+    $request = $ebay->createDataObject('urn:ebay:apis:eBLBaseComponents', 'GetSearchResultsRequestType');
+    $request->Version = 495;
+    $request->Query = 'ipod';
+    $request->createDataObject('Pagination');
+    $request->Pagination->EntriesPerPage = 10;
+
+    try {
+        $results = $ebay->GetSearchResults($request);
+        $total_results = $results->PaginationResult->TotalNumberOfEntries;
+        echo "<b>{$total_results} results</b><br/><br/>";
+        if ($total_results) {
+            foreach ($results->SearchResultItemArray as $search_result_items) {
+                foreach ($search_result_items as $search_result_item) {
+                    echo '<table border="1">';
+                    foreach ($search_result_item->Item as $name => $value) {
+                        echo "<tr><td>{$name}</td><td>";
+                        print_r($value);
+                        echo "</td></tr>";
+                    }
+                    echo '</table></br>';
+                }
+            }
+        }
+    } catch (Exception $e) {
+        echo '<b>Exception: </b>' . $e->getMessage();
+    }
+
+}
+
+?>
+
+<form action="eBayClient.php" method="POST">
+    <b>Query: </b>
+    <input name="query"/>
+    <input type="submit" name="submitquery" value="Submit Query">
+</form>
+
+<?php
+if (array_key_exists('query', $_POST)) {
+    getResults($_POST['query']);
+}
+?>
