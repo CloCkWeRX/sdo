c0978f9a5207ea39b931c32160702f6ef60c94b2
Daniel O'Connor <daniel.oconnor@rpdata.com>
2011-09-05 12:20:59 +0930

    PHPCS

--- SCA/SCA_Logger.php
+++ SCA/SCA_Logger.php
@@ -1,126 +1,41 @@
 <?php
 /**
-* +-----------------------------------------------------------------------------+
-* | (c) Copyright IBM Corporation 2006, 2007.                                   |
-* | All Rights Reserved.                                                        |
-* +-----------------------------------------------------------------------------+
-* | Licensed under the Apache License, Version 2.0 (the "License"); you may not |
-* | use this file except in compliance with the License. You may obtain a copy  |
-* | of the License at -                                                         |
-* |                                                                             |
-* |                   http://www.apache.org/licenses/LICENSE-2.0                |
-* |                                                                             |
-* | Unless required by applicable law or agreed to in writing, software         |
-* | distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
-* | WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
-* | See the License for the specific language governing  permissions and        |
-* | limitations under the License.                                              |
-* +-----------------------------------------------------------------------------+
-* | Author: Graham Charters,                                                    |
-* |         Matthew Peters,                                                     |
-* |         Megan Beynon,                                                       |
-* |         Chris Miller.                                                       |
-* |                                                                             |
-* +-----------------------------------------------------------------------------+
-* $Id: SCA_Logger.php 241789 2007-08-24 15:20:26Z mfp $
-*/
+ * +-----------------------------------------------------------------------------+
+ * | (c) Copyright IBM Corporation 2006, 2007.                                   |
+ * | All Rights Reserved.                                                        |
+ * +-----------------------------------------------------------------------------+
+ * | Licensed under the Apache License, Version 2.0 (the "License"); you may not |
+ * | use this file except in compliance with the License. You may obtain a copy  |
+ * | of the License at -                                                         |
+ * |                                                                             |
+ * |                   http://www.apache.org/licenses/LICENSE-2.0                |
+ * |                                                                             |
+ * | Unless required by applicable law or agreed to in writing, software         |
+ * | distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
+ * | WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
+ * | See the License for the specific language governing  permissions and        |
+ * | limitations under the License.                                              |
+ * +-----------------------------------------------------------------------------+
+ * | Author: Graham Charters,                                                    |
+ * |         Matthew Peters,                                                     |
+ * |         Megan Beynon,                                                       |
+ * |         Chris Miller.                                                       |
+ * |                                                                             |
+ * +-----------------------------------------------------------------------------+
+ * $Id: SCA_Logger.php 241789 2007-08-24 15:20:26Z mfp $
+ *
+ * PHP Version 5
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
 
 require_once 'SCA/SCA_LogInterface.php';
 
-/**
-* Purpose:
-* --------
-* The following class implements, and extends the SCA_LogInterface, to provide
-* methods that control, and enter log information to a file.
-* Information is given in the form of a message that may be categorised in order
-* to provide a measure of weighting to each message. The weighting mechanism also
-* allows a report to be compiled of the msgs that are categorised with the same
-* value.
-* Ancillary methods allow for control of the logging operations, and the current
-* status of the logger to be gathered.
-*
-*
-* Public Methods:
-* ---------------
-* singleSCA_Logger()
-* Return a singleton SCA_Logger.
-*
-* SCA_Logger()
-* The construct method accepts 3 optional arguments, the first two are the directory
-* path, and the filename (without extension) respectively. The third provides an
-* option to delete the existing log file, or continue updating the file with
-* messages.
-* The commands are SCA_LOGGER_DELETE, or SCA_LOGGER_UPDATE, and the command is
-* defaulted to DELETE.
-*
-* setLogLevel()
-* The seetLogLevel function provides a facility to filter out log messages
-* based on their categorisation level. Valid levels are provided by SCA_Logger
-* constants
-*     SCA_LOGGER_CRITICAL
-*     SCA_LOGGER_ERROR
-*     SCA_LOGGER_WARNING
-*     SCA_LOGGER_INFO
-*     SCA_LOGGER_DEBUG
-* The feature allows the log messages to be implanted at strategic points in the
-* code and 'switched' on or off recording their message level.
-* NOTE: 'NONE will stop the logger all the others start the logger.
-*
-* log()
-* The toLog function does what it suggests and logs a message to the log file.
-* An optional parameter allows each message to be categorised with -
-*     SCA_LOGGER_CRITICAL
-*     SCA_LOGGER_ERROR
-*     SCA_LOGGER_WARNING
-*     SCA_LOGGER_INFO
-*     SCA_LOGGER_DEBUG
-* the default is SCA_LOGGER_INFO. Depending on the setting of the
-* categorisation level (see setLogLevel() above) a decision is made
-* as to whether the message is logged or not.
-*
-* fromLog()
-* The fromLog function extracts all the contents of the log file to an array
-* depending on the setting of the optional parameter to select the categorisation of
-* messages that are to be returned. The default setting is SCA_LOGGER_ALL,
-* and the remainder are the same as toLog() above.
-*
-* stopLog()
-* The stopLog function stops logging activity.
-*
-* startLog()
-* The startLog function restarts (the default is 'start') the logger.
-*
-* deleteLog()
-* Remove the current log file from the directory specified in the __construct
-*
-* logStatus()
-* The logStatus function returns an array containing the current state of the Logger
-* The values can be accessed through
-* 'run'    = Current state.
-* 'dir'    = Where the filename is being written to.
-* 'file'   = Filename in which the messages are being recorded.
-* 'catgry' = Level of categories of message being recorded.
-* 'count'  = Number of recorded messages.
-*
-* Private Methods:
-* ----------------
-* _tolog()
-* Format the log entry message, and write it to file.
-*
-* _getMsg()
-* Return all of or the last message in the log.
-*
-* _isLoggable()
-* Provides the filter as to whether a message is going to be written to a file
-* or read back out of the file.
-*
-* _toLevelString()
-* Puts a string representation of the current category into the status array.
-*
-* _fromLevelString()
-* Interprets a declaration and returns the integer equivelent.
-*
-*/
+
 
 /* Service Component Architecture Logger Class                                */
 
@@ -142,25 +57,123 @@ define('SCA_LOGGER_DEBUG', 0x10); // Debug message
 define('SCA_LOGGER_ALL', 0x1f); // all types
 define('SCA_LOGGER_NONE', 0x00); // no types (stop logging)
 
-class SCA_Logger implements  iLogInterface
+/**
+ * Purpose:
+ * --------
+ * The following class implements, and extends the SCA_LogInterface, to provide
+ * methods that control, and enter log information to a file.
+ * Information is given in the form of a message that may be categorised in order
+ * to provide a measure of weighting to each message. The weighting mechanism also
+ * allows a report to be compiled of the msgs that are categorised with the same
+ * value.
+ * Ancillary methods allow for control of the logging operations, and the current
+ * status of the logger to be gathered.
+ *
+ * Public Methods:
+ * ---------------
+ * singleSCA_Logger()
+ * Return a singleton SCA_Logger.
+ *
+ * SCA_Logger()
+ * The construct method accepts 3 optional arguments, the first two are the directory
+ * path, and the filename (without extension) respectively. The third provides an
+ * option to delete the existing log file, or continue updating the file with
+ * messages.
+ * The commands are SCA_LOGGER_DELETE, or SCA_LOGGER_UPDATE, and the command is
+ * defaulted to DELETE.
+ *
+ * setLogLevel()
+ * The seetLogLevel function provides a facility to filter out log messages
+ * based on their categorisation level. Valid levels are provided by SCA_Logger
+ * constants
+ *     SCA_LOGGER_CRITICAL
+ *     SCA_LOGGER_ERROR
+ *     SCA_LOGGER_WARNING
+ *     SCA_LOGGER_INFO
+ *     SCA_LOGGER_DEBUG
+ * The feature allows the log messages to be implanted at strategic points in the
+ * code and 'switched' on or off recording their message level.
+ * NOTE: 'NONE will stop the logger all the others start the logger.
+ *
+ * log()
+ * The toLog function does what it suggests and logs a message to the log file.
+ * An optional parameter allows each message to be categorised with -
+ *     SCA_LOGGER_CRITICAL
+ *     SCA_LOGGER_ERROR
+ *     SCA_LOGGER_WARNING
+ *     SCA_LOGGER_INFO
+ *     SCA_LOGGER_DEBUG
+ * the default is SCA_LOGGER_INFO. Depending on the setting of the
+ * categorisation level (see setLogLevel() above) a decision is made
+ * as to whether the message is logged or not.
+ *
+ * fromLog()
+ * The fromLog function extracts all the contents of the log file to an array
+ * depending on the setting of the optional parameter to select the categorisation of
+ * messages that are to be returned. The default setting is SCA_LOGGER_ALL,
+ * and the remainder are the same as toLog() above.
+ *
+ * stopLog()
+ * The stopLog function stops logging activity.
+ *
+ * startLog()
+ * The startLog function restarts (the default is 'start') the logger.
+ *
+ * deleteLog()
+ * Remove the current log file from the directory specified in the __construct
+ *
+ * logStatus()
+ * The logStatus function returns an array containing the current state of the Logger
+ * The values can be accessed through
+ * 'run'    = Current state.
+ * 'dir'    = Where the filename is being written to.
+ * 'file'   = Filename in which the messages are being recorded.
+ * 'catgry' = Level of categories of message being recorded.
+ * 'count'  = Number of recorded messages.
+ *
+ * Private Methods:
+ * ----------------
+ * _tolog()
+ * Format the log entry message, and write it to file.
+ *
+ * _getMsg()
+ * Return all of or the last message in the log.
+ *
+ * _isLoggable()
+ * Provides the filter as to whether a message is going to be written to a file
+ * or read back out of the file.
+ *
+ * _toLevelString()
+ * Puts a string representation of the current category into the status array.
+ *
+ * _fromLevelString()
+ * Interprets a declaration and returns the integer equivelent.
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
+class SCA_Logger implements iLogInterface
 {
     /* Singleton instance of the logger.                                  */
-    private static $loghandle   = null;
+    protected static $loghandle   = null;
 
-    private        $run         = SCA_LOGGER_STOP;
-    private        $catlevel    = SCA_LOGGER_ALL;
+    protected $run         = SCA_LOGGER_STOP;
+    protected $catlevel    = SCA_LOGGER_ALL;
 
-    private        $levelindex  = 4;
+    protected $levelindex  = 4;
 
     /* File path information                                              */
-    const          LASTMSG      = 'EOF';
-    private        $dirpath     = ".";
-    private        $file        = "SCA";
-    private        $extn        = "log";
-    private        $logfile     = "";
+    const LASTMSG      = 'EOF';
+    protected $dirpath     = ".";
+    protected $file        = "SCA";
+    protected $extn        = "log";
+    protected $logfile     = "";
 
     /* Line count of the log messages                                     */
-    private        $msg_index    = 0;
+    protected $msg_index    = 0;
 
     /**
      * Create a singleton Logger.
@@ -194,17 +207,20 @@ class SCA_Logger implements  iLogInterface
     public function log($txtentry, $file = "", $line = "", $level = null)
     {
         /* Exit quickly when logging has been stopped.                    */
-        if ($this->run) {
-            /* After ensuring that the level is valid ...                 */
-            if ($level !== null) {
-                if (($categorisation = $this->_isLoggable($level)) !== null) {
-                    $this->_tolog($txtentry, $file, $line, $categorisation);
-                }
-            } else {
-                /* ... otherwise use the default level                    */
-                $this->_tolog($txtentry, $file, $line, $this->_toLevelString(SCA_LOGGER_INFO));
-            }
+        if (!$this->run) {
+            return;
+        }
 
+        /* After ensuring that the level is valid ...                 */
+        if ($level !== null) {
+            if (($categorisation = $this->_isLoggable($level)) !== null) {
+                $this->_tolog($txtentry, $file, $line, $categorisation);
+            }
+        } else {
+            /* ... otherwise use the default level                    */
+            $this->_tolog(
+                $txtentry, $file, $line, $this->_toLevelString(SCA_LOGGER_INFO)
+            );
         }
     }
 
@@ -226,7 +242,8 @@ class SCA_Logger implements  iLogInterface
     public function startLog()
     {
         if ($this->run == SCA_LOGGER_START) {
-            // we are already started; this is a second or subsequent call to startLog() - do nothing
+            // we are already started; this is a second
+            // or subsequent call to startLog() - do nothing
             return;
         }
 
@@ -242,8 +259,9 @@ class SCA_Logger implements  iLogInterface
 
         }
 
-        if ($file !== null )
-        $this->file = $file;
+        if ($file !== null) {
+            $this->file = $file;
+        }
 
         $this->logfile = "{$this->dirpath}/{$this->file}.{$this->extn}";
 
@@ -380,7 +398,9 @@ class SCA_Logger implements  iLogInterface
 
             $this->_findCallingInfo($backtrace, $calling_class, $calling_method);
 
-            $stack_depth = substr('........................................', 1, count($backtrace));
+            $stack_depth = substr(
+                '........................................', 1, count($backtrace)
+            );
 
         }
 
@@ -390,8 +410,10 @@ class SCA_Logger implements  iLogInterface
         $u_secs = gettimeofday();
         $m_secs = $u_secs['usec']/1000;
 
-        $log_msg = sprintf("[%3d] %s %s::%3d %s%s::%s - %s\n", $this->msg_index, $level,
-                            $time, $m_secs, $stack_depth, $calling_class, $calling_method, $msg);
+        $log_msg = sprintf(
+            "[%3d] %s %s::%3d %s%s::%s - %s\n", $this->msg_index, $level,
+            $time, $m_secs, $stack_depth, $calling_class, $calling_method, $msg
+        );
 
         file_put_contents($this->logfile, $log_msg, FILE_APPEND);
 
@@ -426,8 +448,9 @@ class SCA_Logger implements  iLogInterface
 
                     /* Jump out when the there is no message (normally    */
                     /* EOF)                                               */
-                    if ((strlen($msg)) === 0 )
-                    break;
+                    if ((strlen($msg)) === 0) {
+                        break;
+                    }
 
                     /* Save all the messages to an array or just the last */
                     /* one                                                */
@@ -437,8 +460,9 @@ class SCA_Logger implements  iLogInterface
                             $logList[$i++] = $msg;
 
                         } else {
-                            if ((strpos($msg, $cat)) !== false )
-                            $logList[$i++] = $msg;
+                            if ((strpos($msg, $cat)) !== false) {
+                                $logList[$i++] = $msg;
+                            }
 
                         }
 
@@ -519,7 +543,7 @@ class SCA_Logger implements  iLogInterface
             SCA_LOGGER_DEBUG    => 'Debug',
             SCA_LOGGER_ALL      => 'AllLevels',
             SCA_LOGGER_NONE     => 'NoLevels'
-       );
+        );
 
         if (key_exists($keyvalue, $leveltext)) {
             return $leveltext[$keyvalue];
@@ -545,7 +569,7 @@ class SCA_Logger implements  iLogInterface
             'Debug'     => SCA_LOGGER_DEBUG,
             'AllLevels' => SCA_LOGGER_ALL,
             'NoLevels'  => SCA_LOGGER_NONE
-       );
+        );
 
         if (key_exists($keyvalue, $textlevel)) {
             return $textlevel[$keyvalue];
