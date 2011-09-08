bd9bc5feca4d5dbcbc93b0552113813bf4fb5d1c
Daniel O'Connor <daniel.oconnor@rpdata.com>
2011-09-08 11:11:11 +0930

    PHPCS

--- SCA/Bindings/tuscany/SCA_TuscanyProxy.php
+++ SCA/Bindings/tuscany/SCA_TuscanyProxy.php
@@ -1,50 +1,62 @@
 <?php
-/*
-+-----------------------------------------------------------------------------+
-| (c) Copyright IBM Corporation 2006.                                         |
-| All Rights Reserved.                                                        |
-+-----------------------------------------------------------------------------+
-| Licensed under the Apache License, Version 2.0 (the "License"); you may not |
-| use this file except in compliance with the License. You may obtain a copy  |
-| of the License at -                                                         |
-|                                                                             |
-|                   http://www.apache.org/licenses/LICENSE-2.0                |
-|                                                                             |
-| Unless required by applicable law or agreed to in writing, software         |
-| distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
-| WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
-| See the License for the specific language governing  permissions and        |
-| limitations under the License.                                              |
-+-----------------------------------------------------------------------------+
-| Author: Graham Charters,                                                    |
-|         Matthew Peters,                                                     |
-|         Megan Beynon,                                                       |
-|         Chris Miller,                                                       |
-|         Caroline Maynard,                                                   |
-|         Simon Laws                                                          |
-+-----------------------------------------------------------------------------+
-$Id: SCA_TuscanyProxy.php 234945 2007-05-04 15:05:53Z mfp $
-*/
+/**
+ * +-----------------------------------------------------------------------------+
+ * | (c) Copyright IBM Corporation 2006.                                         |
+ * | All Rights Reserved.                                                        |
+ * +-----------------------------------------------------------------------------+
+ * | Licensed under the Apache License, Version 2.0 (the "License"); you may not |
+ * | use this file except in compliance with the License. You may obtain a copy  |
+ * | of the License at -                                                         |
+ * |                                                                             |
+ * |                   http://www.apache.org/licenses/LICENSE-2.0                |
+ * |                                                                             |
+ * | Unless required by applicable law or agreed to in writing, software         |
+ * | distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
+ * | WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
+ * | See the License for the specific language governing  permissions and        |
+ * | limitations under the License.                                              |
+ * +-----------------------------------------------------------------------------+
+ * | Author: Graham Charters,                                                    |
+ * |         Matthew Peters,                                                     |
+ * |         Megan Beynon,                                                       |
+ * |         Chris Miller,                                                       |
+ * |         Caroline Maynard,                                                   |
+ * |         Simon Laws                                                          |
+ * +-----------------------------------------------------------------------------+
+ * $Id: SCA_TuscanyProxy.php 234945 2007-05-04 15:05:53Z mfp $
+ *
+ * PHP Version 5
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
 
 /**
-*
-*/
+ * Tuscany proxy
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
+class SCA_TuscanyProxy
+{
 
-
-class SCA_TuscanyProxy {
-
-    private $containing_class_name          = null;
-    private $reference_name                 = null;
+    protected $containing_class_name          = null;
+    protected $reference_name                 = null;
 
     /**
-      * Create the local proxy to the service given as an argument.
-      *
-      * @param string $absolute_path_to_component
-      */
+     * Create the local proxy to the service given as an argument.
+     *
+     * @param string $reference_name Reference name
+     */
     public function __construct($reference_name)
     {
         $this->reference_name = $reference_name;
-
     }
 
     /**
@@ -52,11 +64,22 @@ class SCA_TuscanyProxy {
      * represents the whole doc comment that this proxy
      * is configured with. We don't use it here at the
      * moment.
+     *
+     * @param string $reference_type Reference type
+     *
+     * @return null
      */
     public function addReferenceType($reference_type)
     {
     }
 
+    /**
+     * Add containing class name
+     *
+     * @param string $class_name Class name
+     *
+     * @return null
+     */
     public function addContainingClassName($class_name)
     {
         $this->containing_class_name = $class_name;
@@ -65,27 +88,40 @@ class SCA_TuscanyProxy {
     /**
      * Invoke the method name in the target service.
      *
-     * @param string $method_name
-     * @param array $arguments
+     * @param string $method_name Method name
+     * @param array  $arguments   Arguments
+     *
      * @return mixed
      */
     public function __call($method_name, $arguments)
     {
         // Invoke the Tuscany service
 
-        $return = SCA_Tuscany::invoke($this->containing_class_name,
-                                     $this->reference_name,
-                                     $method_name,
-                                     $arguments);
+        $return = SCA_Tuscany::invoke(
+            $this->containing_class_name,
+            $this->reference_name,
+            $method_name,
+            $arguments
+        );
+
         return $return;
     }
 
-    public function createDataObject($namespace_uri, $type_name )
+    /**
+     * Create a data object
+     *
+     * @param string $namespace_uri Namespace URI
+     * @param string $type_name     Type name
+     *
+     * @return mixed
+     */
+    public function createDataObject($namespace_uri, $type_name)
     {
         try {
             return SCA_Helper::createDataObject(
-                $namespace_uri, $type_name, $this->component_class_name);
-        } catch( Exception $e ) {
+                $namespace_uri, $type_name, $this->component_class_name
+            );
+        } catch (Exception $e) {
             throw new SCA_RuntimeException($e->getMessage());
         }
         // following return logically unecessary but keeps the ZS code
--- SCA/Bindings/tuscany/SCA_TuscanyWrapper.php
+++ SCA/Bindings/tuscany/SCA_TuscanyWrapper.php
@@ -1,57 +1,72 @@
 <?php
-/*
-+-----------------------------------------------------------------------------+
-| (c) Copyright IBM Corporation 2006.                                         |
-| All Rights Reserved.                                                        |
-+-----------------------------------------------------------------------------+
-| Licensed under the Apache License, Version 2.0 (the "License"); you may not |
-| use this file except in compliance with the License. You may obtain a copy  |
-| of the License at -                                                         |
-|                                                                             |
-|                   http://www.apache.org/licenses/LICENSE-2.0                |
-|                                                                             |
-| Unless required by applicable law or agreed to in writing, software         |
-| distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
-| WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
-| See the License for the specific language governing  permissions and        |
-| limitations under the License.                                              |
-+-----------------------------------------------------------------------------+
-| Author: Graham Charters,                                                    |
-|         Matthew Peters,                                                     |
-|         Megan Beynon,                                                       |
-|         Chris Miller,                                                       |
-|         Caroline Maynard,                                                   |
-|         Simon Laws                                                          |
-+-----------------------------------------------------------------------------+
-$Id: SCA_TuscanyWrapper.php 234945 2007-05-04 15:05:53Z mfp $
-*/
+/**
+ * +-----------------------------------------------------------------------------+
+ * | (c) Copyright IBM Corporation 2006.                                         |
+ * | All Rights Reserved.                                                        |
+ * +-----------------------------------------------------------------------------+
+ * | Licensed under the Apache License, Version 2.0 (the "License"); you may not |
+ * | use this file except in compliance with the License. You may obtain a copy  |
+ * | of the License at -                                                         |
+ * |                                                                             |
+ * |                   http://www.apache.org/licenses/LICENSE-2.0                |
+ * |                                                                             |
+ * | Unless required by applicable law or agreed to in writing, software         |
+ * | distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
+ * | WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
+ * | See the License for the specific language governing  permissions and        |
+ * | limitations under the License.                                              |
+ * +-----------------------------------------------------------------------------+
+ * | Author: Graham Charters,                                                    |
+ * |         Matthew Peters,                                                     |
+ * |         Megan Beynon,                                                       |
+ * |         Chris Miller,                                                       |
+ * |         Caroline Maynard,                                                   |
+ * |         Simon Laws                                                          |
+ * +-----------------------------------------------------------------------------+
+ * $Id: SCA_TuscanyWrapper.php 234945 2007-05-04 15:05:53Z mfp $
+ *
+ * PHP Version 5
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
 
 /**
-* This class is called when a PHP service is invoked from the CPP SCA
-* runtime. It decides how the service is implemented
-*   php file
-*   php function
-*   php class function
-*   php sca service
-* It then marshalls the parameters correctly in order to make the call
-* and return the response.
-*/
+ * This class is called when a PHP service is invoked from the CPP SCA
+ * runtime. It decides how the service is implemented
+ *   php file
+ *   php function
+ *   php class function
+ *   php sca service
+ * It then marshalls the parameters correctly in order to make the call
+ * and return the response.
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
+class SCA_TuscanyWrapper
+{
 
-
-
-class SCA_TuscanyWrapper {
-
-    private $mediator        = null;
-    private $service         = null;
-    private $component_name  = null;
-    private $class_name      = null;
-    private $method_name     = null;
+    protected $mediator        = null;
+    protected $service         = null;
+    protected $component_name  = null;
+    protected $class_name      = null;
+    protected $method_name     = null;
 
     /**
      * Create the service wrapper for a SCA Component.
      *
-     * @param string $component_name
-     * @param string $wsdl_filename
+     * @param string $mediator       Mediator
+     * @param string $component_name Component name
+     * @param string $class_name     Class name
+     * @param string $method_name    Method name
+     * @param string $arg_array      Arguments
      */
     public function __construct($mediator, $component_name, $class_name, $method_name, $arg_array)
     {
@@ -67,12 +82,12 @@ class SCA_TuscanyWrapper {
 
         // if a class name has not been provided
         // then this may just be a script.
-        if ($class_name == null ) {
-         //   $arg_array = $this->mediator->getArgArray();
+        if ($class_name == null) {
+            // $arg_array = $this->mediator->getArgArray();
 
             // get the arguments and put them into the request
             $index = 0;
-            foreach ($arg_array as $arg_val ) {
+            foreach ($arg_array as $arg_val) {
                 $_REQUEST[$index] = $arg_val;
                 $index = $index + 1;
             }
@@ -88,20 +103,23 @@ class SCA_TuscanyWrapper {
     }/* End service wrapper constructor  */
 
 
+    /**
+     * Invoke
+     *
+     * @return string
+     */
     public function invoke()
     {
         SCA::$logger->log('Entering');
 
-//component_name, string reference_name, string method_name, array arguments
-//           Reflection::export(new ReflectionObject($this->mediator));
+        //component_name, string reference_name, string method_name, array arguments
+        //           Reflection::export(new ReflectionObject($this->mediator));
 
         try {
             // get the arguments from the mediator
             $arg_array = $this->mediator->getArgArray();
 
-//var_dump($arg_array);
-
-            if ($this->class_name != null ) {
+            if ($this->class_name != null) {
                 SCA::$logger->log('Attempt to create a service');
                 $this->service = new $this->class_name();
                 $reflection    = new ReflectionObject($this->service);
@@ -109,7 +127,7 @@ class SCA_TuscanyWrapper {
 
                 // if it's an SCA service recreate the instance
                 // with all the references filled in
-                if ($reader->isService() ) {
+                if ($reader->isService()) {
                     SCA::$logger->log('Create SCA service');
                     $this->service = SCA::createInstanceAndFillInReferences($this->class_name);
                 } else {
@@ -118,7 +136,7 @@ class SCA_TuscanyWrapper {
             }
 
             //invoke the function
-            if ($this->service != null ) {
+            if ($this->service != null) {
                 SCA::$logger->log('Invoke service as PHP object');
                 // it's a class with a member function
                 // so call it. Even if it's an SCA service
@@ -126,18 +144,21 @@ class SCA_TuscanyWrapper {
                 // at this point. We just want the references
                 // that will have been set up on initialization
                 // above.
-                $return = call_user_func_array(array($this->service,
-                                                     $this->method_name),
-                                               $arg_array);
+                $return = call_user_func_array(
+                    array($this->service, $this->method_name),
+                    $arg_array
+                );
 
-            } else if ( function_exists($this->method_name) ) {
+            } else if (function_exists($this->method_name)) {
                 SCA::$logger->log('Invoke service as PHP function');
                 // it's not just a script so turn
                 // off output buffering and then call
                 // the function
                 ob_end_clean();
-                $return = call_user_func_array($this->method_name,
-                                               $arg_array);
+                $return = call_user_func_array(
+                    $this->method_name,
+                    $arg_array
+                );
             } else {
                 SCA::$logger->log('Invoke service as PHP script');
                 // the script will have already executed
@@ -148,7 +169,7 @@ class SCA_TuscanyWrapper {
                 ob_end_clean();
             }
 
-        } catch ( Exception $e ) {
+        } catch (Exception $e) {
             SCA::$logger->log('Caught '.$e->getMessage());
             throw $e;
         }
