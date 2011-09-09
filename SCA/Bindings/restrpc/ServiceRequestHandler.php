ff31d248cfc2f11f092226f87a7e8defbc0a3bf6
Daniel O'Connor <daniel.oconnor@rpdata.com>
2011-09-09 12:41:58 +0930

    PHPCS

--- SCA/Bindings/restrpc/ServiceRequestHandler.php
+++ SCA/Bindings/restrpc/ServiceRequestHandler.php
@@ -1,34 +1,50 @@
 <?php
 /**
-+-----------------------------------------------------------------------------+
-| (c) Copyright IBM Corporation 2006.                                         |
-| All Rights Reserved.                                                        |
-+-----------------------------------------------------------------------------+
-| Licensed under the Apache License, Version 2.0 (the "License"); you may not |
-| use this file except in compliance with the License. You may obtain a copy  |
-| of the License at -                                                         |
-|                                                                             |
-|                   http://www.apache.org/licenses/LICENSE-2.0                |
-|                                                                             |
-| Unless required by applicable law or agreed to in writing, software         |
-| distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
-| WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
-| See the License for the specific language governing  permissions and        |
-| limitations under the License.                                              |
-+-----------------------------------------------------------------------------+
-| Author: Graham Charters,                                                    |
-|         Matthew Peters,                                                     |
-|         Megan Beynon,                                                       |
-|         Chris Miller,                                                       |
-|         Caroline Maynard,                                                   |
-|         Simon Laws                                                          |
-+-----------------------------------------------------------------------------+
-*/
+ * +-----------------------------------------------------------------------------+
+ * | (c) Copyright IBM Corporation 2006.                                         |
+ * | All Rights Reserved.                                                        |
+ * +-----------------------------------------------------------------------------+
+ * | Licensed under the Apache License, Version 2.0 (the "License"); you may not |
+ * | use this file except in compliance with the License. You may obtain a copy  |
+ * | of the License at -                                                         |
+ * |                                                                             |
+ * |                   http://www.apache.org/licenses/LICENSE-2.0                |
+ * |                                                                             |
+ * | Unless required by applicable law or agreed to in writing, software         |
+ * | distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
+ * | WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
+ * | See the License for the specific language governing  permissions and        |
+ * | limitations under the License.                                              |
+ * +-----------------------------------------------------------------------------+
+ * | Author: Graham Charters,                                                    |
+ * |         Matthew Peters,                                                     |
+ * |         Megan Beynon,                                                       |
+ * |         Chris Miller,                                                       |
+ * |         Caroline Maynard,                                                   |
+ * |         Simon Laws                                                          |
+ * +-----------------------------------------------------------------------------+
+ * PHP Version 5
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
 
-include "SCA/Bindings/restrpc/Wrapper.php";
-include "SCA/Bindings/restrpc/Server.php";
+require_once "SCA/Bindings/restrpc/Wrapper.php";
+require_once "SCA/Bindings/restrpc/Server.php";
 
-class SCA_Bindings_restrpc_ServiceRequestHandler
+/**
+ * RestRPC ServiceRequestHandler
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
+class SCA_Bindings_Restrpc_ServiceRequestHandler
 {
     /**
      * Handle
--- SCA/Bindings/restrpc/Wrapper.php
+++ SCA/Bindings/restrpc/Wrapper.php
@@ -1,44 +1,59 @@
 <?php
 /**
-+-----------------------------------------------------------------------------+
-| (c) Copyright IBM Corporation 2006.                                         |
-| All Rights Reserved.                                                        |
-+-----------------------------------------------------------------------------+
-| Licensed under the Apache License, Version 2.0 (the "License"); you may not |
-| use this file except in compliance with the License. You may obtain a copy  |
-| of the License at -                                                         |
-|                                                                             |
-|                   http://www.apache.org/licenses/LICENSE-2.0                |
-|                                                                             |
-| Unless required by applicable law or agreed to in writing, software         |
-| distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
-| WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
-| See the License for the specific language governing  permissions and        |
-| limitations under the License.                                              |
-+-----------------------------------------------------------------------------+
-| Author: Graham Charters,                                                    |
-|         Matthew Peters,                                                     |
-|         Megan Beynon,                                                       |
-|         Chris Miller,                                                       |
-|         Caroline Maynard,                                                   |
-|         Simon Laws                                                          |
-+-----------------------------------------------------------------------------+
-*/
-
-
-class SCA_Bindings_restrpc_Wrapper
+ * +-----------------------------------------------------------------------------+
+ * | (c) Copyright IBM Corporation 2006.                                         |
+ * | All Rights Reserved.                                                        |
+ * +-----------------------------------------------------------------------------+
+ * | Licensed under the Apache License, Version 2.0 (the "License"); you may not |
+ * | use this file except in compliance with the License. You may obtain a copy  |
+ * | of the License at -                                                         |
+ * |                                                                             |
+ * |                   http://www.apache.org/licenses/LICENSE-2.0                |
+ * |                                                                             |
+ * | Unless required by applicable law or agreed to in writing, software         |
+ * | distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   |
+ * | WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            |
+ * | See the License for the specific language governing  permissions and        |
+ * | limitations under the License.                                              |
+ * +-----------------------------------------------------------------------------+
+ * | Author: Graham Charters,                                                    |
+ * |         Matthew Peters,                                                     |
+ * |         Megan Beynon,                                                       |
+ * |         Chris Miller,                                                       |
+ * |         Caroline Maynard,                                                   |
+ * |         Simon Laws                                                          |
+ * +-----------------------------------------------------------------------------+
+ *
+ * PHP Version 5
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
+/**
+ * RestRPC Wrapper
+ *
+ * @category SCA
+ * @package  SCA_SDO
+ * @author   Matthew Peters <mfp@php.net>
+ * @license  Apache http://www.apache.org/licenses/LICENSE-2.0
+ * @link     http://www.osoa.org/display/PHP/
+ */
+class SCA_Bindings_Restrpc_Wrapper
 {
 
-    private $class_name     = null;
-    private $class_instance = null;
-    private $xml_das        = null;
+    protected $class_name     = null;
+    protected $class_instance = null;
+    protected $xml_das        = null;
 
     /**
      * Create the service wrapper for an SCA Component. In the event that the
      * mapping of the SCA Component methods the base_class and xmldas types are
      * set to null.
      *
-     * @param string $class_name
+     * @param string $class_name Class name
      */
     public function __construct($class_name)
     {
@@ -56,17 +71,29 @@ class SCA_Bindings_restrpc_Wrapper
 
         SCA::$logger->log("Exiting Constructor");
 
-    }/* End service wrapper constructor  */
+    }
 
-
+    /**
+     * Get XML DAS
+     *
+     * @return XML_DAS
+     */
     public function getXmlDas()
     {
         return $this->xml_das;
     }
 
+    /**
+     * Get parameters for method
+     *
+     * @param string $method_name Method name
+     *
+     * @return array
+     */
     public function getParametersForMethod($method_name)
     {
-        $reader              = new SCA_AnnotationReader($this->class_instance);
+        $reader = new SCA_AnnotationReader($this->class_instance);
+
         $service_description = $reader->reflectService();
 
         $operations = $service_description->operations;
@@ -86,7 +113,12 @@ class SCA_Bindings_restrpc_Wrapper
      * Then pass to the method
      * Then wrap the return value back into an SDO. The element name is ...Response with a
      * property ...Return which contains the return value.
-    */
+     *
+     * @param string $method_name Method name
+     * @param string $arguments   Arguments
+     *
+     * @return mixed
+     */
     public function __call($method_name, $arguments=null)
     {
         SCA::$logger->log("Entering __call");
@@ -96,15 +128,17 @@ class SCA_Bindings_restrpc_Wrapper
         //allowing null so that method that takes no args can be called
 
         SCA::$logger->log("About to do call_user_func_array with method $method_name and arguments $arguments \n");
-        $return = call_user_func_array(array(&$this->class_instance,
-                                             $method_name),
-                                       $arguments);
+        $return = call_user_func_array(
+            array(&$this->class_instance, $method_name),
+            $arguments
+        );
+
         SCA::$logger->log("Got return back from call_user_func_array: $return");
 
         SCA::$logger->log("Exiting __call");
 
         return $return;
-    }/* End of call function                                                  */
+    }
 
-}/* End Service Wrapper class                                                 */
+}
 
